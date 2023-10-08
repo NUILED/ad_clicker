e411e10d5cbca2c595afed5a8486b25e4ab2d464
Nulled <64336136+NuIled@users.noreply.github.com>
2023-10-07 21:09:03 -0400

    Update p.py

--- p.py
+++ p.py
@@ -58,14 +58,14 @@ def run_browser():
                         print(page.url)
                         time.sleep(random.randint(8, 20))
                         page.evaluate('window.scrollBy(0, window.innerHeight);')
-                        break
                     else:
                         print("not clicked")
             context.close()
             browser.close()
 
 if __name__ == "__main__":
-    keywords = ["ambulance casablanca", "urgence casablanca", "ambulance","numero Ambulance","medecin a domicile","sos medecin casablanca","docteur a domicile"]
+    # keywords = ["ambulance casablanca", "urgence casablanca", "ambulance","numero Ambulance"]
+    keywords = ["medecin a domicile","sos medecin casablanca","docteur a domicile"]
     # target_websites = ["https://chronosecours.ma/ambulance/casablanca","https://chronosecours.ma/ambulance/ambulance","https://chronosecours.ma/ambulance/Maroc","https://chronosecours.ma","https://urgences-maroc.com/ambulance","https://sosambulance24.ma","https://sos-medecin.ma"]
     target_websites = ["https://sosambulance24.ma/","https://sos-medecin.ma"]
 
